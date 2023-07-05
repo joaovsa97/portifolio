871889211c1b7dd68b2a1ec0829499ec32828e9e
joaovsa97 <joaos.azevedo10@gmail.com>
2023-07-05 13:52:37 -0300

    create: project structure and navbar component and style.

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import "./style.scss"
+import logoLight from "../../assets/logo-light.svg"
+import patternLight from "../../assets/navbar-pattern-light.svg"
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <img className="logo" src={logoLight} alt="logo" />
+      <div className="navigation">
+        <button>About Me</button>
+        <button>My Projects</button>
+        <button>Contact Me</button>
+      </div>
+      <img className="pattern" src={patternLight} alt="" />
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/pages/home/index.jsx
@@ -0,0 +1,9 @@
+import "./style.scss"
+
+const Home = () => {
+  return (
+    <div className='container'>index</div>
+  )
+}
+
+export default Home
